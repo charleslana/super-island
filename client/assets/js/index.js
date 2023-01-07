3086ed9ad2687fc9edb3f058486d7f044750be30
Charles <charleslanop@gmail.com>
2023-01-07 17:01:27 -0300

    update

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -2,3 +2,12 @@
 //   e.preventDefault();
 //   e.returnValue = '';
 // });
+
+addEventListener('DOMContentLoaded', () => {
+  changeYear();
+});
+
+function changeYear() {
+  const year = new Date().getFullYear();
+  document.querySelectorAll('.year')[0].innerHTML = year;
+}
